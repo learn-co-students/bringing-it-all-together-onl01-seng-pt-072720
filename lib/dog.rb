e0e25d3ece81c9b17552cf6f85a9df0e2abae259
aarongoen <aarongoen2.0@gmail.com>
2020-10-01 15:01:58 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,7 @@ class Dog
     
     attr_accessor :name, :breed, :id
 
-    def attributes(name:, breed:, id:=nil)
+    def attributes(name:, breed:, id:)
         @name = name
         @breed = breed
         @id = id
@@ -32,26 +32,64 @@ class Dog
         DB[:conn].execute(sql)
     end
 
-    # def new_from_db
-    #     attributes
-    # end 
+    def save
+        sql = 'INSERT INTO dogs (name, breed) VALUES (?, ?);'
+        DB[:conn].execute(sql, self.name, self.breed)
+        # binding.pry
+        @id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs;')[0][0]
+        self
+    end
 
-    # def save
-    #     sql = 'INSERT INTO dogs (name, breed) VALUES (?, ?);'
-    #     DB[:conn].execute(sql, self.name, self.breed)
-    #     # binding.pry
-    #     DB[:conn].execute('SELECT last_insert_rowid() FROM dogs;')[0]
-    #     self
-    # end
+    def self.create(attributes)
+        dog = self.new(attributes)
+        dog.save
+    end
 
+    def self.new_from_db(row)
+        attribute_hash = {
+            :id => row[0], #Is this matching up the key with the value of the first element of the argument's array?
+            :name => row[1],
+            :breed => row[2]
+        }
+        self.new(attribute_hash)
+    end 
 
+    def self.find_by_id(id)
+        sql = "SELECT id FROM dogs WHERE id = ?"
+        
+        DB[:conn].execute(sql, id).map do |row|
+            self.new_from_db(row)
+        end.first #Why "first"?
+    end
 
- 
+    def self.find_or_create_by(name:, breed:)
+        sql = <<-SQL
+          SELECT * FROM dogs
+          WHERE name = ? AND breed = ?
+          SQL
     
-    # table_check_sql <<=SQL
-    # "SELECT tbl_name 
-    # FROM sqlite_master 
-    # WHERE type='table' 
-    # AND tbl_name='dogs';"
-    # SQL
-end
+          dog = DB[:conn].execute(sql, name, breed).first
+
+          if dog
+            new_dog = self.new_from_db(dog)
+          else
+            new_dog = self.create({:name => name, :breed => breed})
+          end
+          new_dog #Why do I have to return? Just to see that there is a result?
+      end
+
+    def self.find_by_name(name)
+        sql = "SELECT * FROM dogs WHERE name = ?"
+        
+        DB[:conn].execute(sql, name).map do |row|
+            self.new_from_db(row)
+        end.first #Why "first"?
+    end
+
+    def update
+        sql = 'UPDATE dogs SET name = ?, breed = ? WHERE id = ?'
+
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+
+end 
